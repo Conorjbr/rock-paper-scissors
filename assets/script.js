7e14999e847a2388388b9979f1d63292f9315429
Conorjbr <cb@live.ie>
2023-05-09 21:03:38 +0000

    Setup main game variables

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('.button')
+let userChoice
+let computerChoice
+let result
+let userScore
+let computerScore = 0;
