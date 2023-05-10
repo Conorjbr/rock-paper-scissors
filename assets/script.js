bfe9d0aa7f7749075d15875fce915acbf0e4b957
Conorjbr <cb@live.ie>
2023-05-10 07:17:59 +0000

    Setup event listener for user choices and display to DOM

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,21 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('.button')
+const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
 let result
 let userScore
 let computerScore
 
-//Add event listener for user choice
+//Event listener for user choice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+  }))
+  
 
 //Game functions
 
